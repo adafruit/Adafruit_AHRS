9810fe062274bda4eb09cd89c7ff632158af5de0
Lady Ada <limor@ladyada.net>
2020-02-01 22:40:47 -0500

    USB and Mahony examples are now superceded by one calibrated_orientation example

--- examples/calibrated_orientation/calibrated_orientation.ino
+++ examples/calibrated_orientation/calibrated_orientation.ino
@@ -1,4 +1,4 @@
-// Full orientation sensing using NXP's advanced sensor fusion algorithm.
+// Full orientation sensing using NXP/Madge.
 //
 // You *must* perform a magnetic calibration before this code will work.
 //
@@ -7,26 +7,25 @@
 // Based on  https://github.com/PaulStoffregen/NXPMotionSense with adjustments
 // to Adafruit Unified Sensor interface
 
-#include <Adafruit_LSM6DS33.h>
-#include <Adafruit_LIS3MDL.h>
 #include <Adafruit_Sensor_Calibration.h>
 #include <Adafruit_AHRS.h>
 
-uint32_t timestamp;
+Adafruit_Sensor *accelerometer, *gyroscope, *magnetometer;
 
-// change to any 
-Adafruit_LSM6DS33 lsm6ds33;
-Adafruit_LIS3MDL lis3mdl;
+// uncomment one combo 9-DoF!
+#include "LSM6DS_LIS3MDL.h"  // can adjust to LSM6DS33, LSM6DS3U, LSM6DSOX...
+//#include "LSM9DS1.h"
+//#include "NXP_FXOS_FXAS.h"  // NXP 9-DoF breakout
 
 Adafruit_Sensor_Calibration cal;
 
 // pick your filter! slower == better quality output
-
-//Adafruit_NXPSensorFusion filter; // slowest
+Adafruit_NXPSensorFusion filter; // slowest
 //Adafruit_Madgwick filter;  // faster than NXP
-Adafruit_Mahony filter;  // fastest
+//Adafruit_Mahony filter;  // fastest
 
 #define FILTER_UPDATE_RATE_HZ 100
+uint32_t timestamp;
 
 void setup() {
   Serial.begin(115200);
@@ -38,26 +37,20 @@ void setup() {
     Serial.println("No calibration loaded/found");
   }
 
-  if (!lsm6ds33.begin_I2C() || ! lis3mdl.begin_I2C()) {
-    Serial.println("Failed to find LSM6DS33/LIS3MDL chips");
-    while (1) {
-      delay(10);
-    }
+  if (!init_sensors()) {
+    Serial.println("Failed to find sensors");
+    while (1) delay(10);
   }
-  // set lowest range
-  lsm6ds33.setAccelRange(LSM6DS_ACCEL_RANGE_2_G);
-  lsm6ds33.setGyroRange(LSM6DS_GYRO_RANGE_250_DPS);
-  lis3mdl.setRange(LIS3MDL_RANGE_4_GAUSS);
+  
+  accelerometer->printSensorDetails();
+  gyroscope->printSensorDetails();
+  magnetometer->printSensorDetails();
 
-  // set slightly above refresh rate
-  lsm6ds33.setAccelDataRate(LSM6DS_RATE_104_HZ);
-  lsm6ds33.setGyroDataRate(LSM6DS_RATE_104_HZ);
-  lis3mdl.setDataRate(LIS3MDL_DATARATE_1000_HZ);
-  lis3mdl.setPerformanceMode(LIS3MDL_MEDIUMMODE);
-  lis3mdl.setOperationMode(LIS3MDL_CONTINUOUSMODE);
-
+  setup_sensors();
   filter.begin(FILTER_UPDATE_RATE_HZ);
   timestamp = millis();
+
+  Wire.setClock(400000); // 400KHz
 }
 
 
@@ -70,9 +63,10 @@ void loop() {
   }
   timestamp = millis();
   // Read the motion sensors
-  sensors_event_t accel, gyro, temp, mag;
-  lsm6ds33.getEvent(&accel, &gyro, &temp);
-  lis3mdl.getEvent(&mag);
+  sensors_event_t accel, gyro, mag;
+  accelerometer->getEvent(&accel);
+  gyroscope->getEvent(&gyro);
+  magnetometer->getEvent(&mag);
 
   cal.calibrate(mag);
   cal.calibrate(accel);
