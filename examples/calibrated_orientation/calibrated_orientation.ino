261423f6115cda90036f5464801432840b30cfbe
Lady Ada <limor@ladyada.net>
2020-02-16 17:42:33 -0500

    make LSM6DS the defaults

--- examples/calibrated_orientation/calibrated_orientation.ino
+++ examples/calibrated_orientation/calibrated_orientation.ino
@@ -13,8 +13,8 @@
 Adafruit_Sensor *accelerometer, *gyroscope, *magnetometer;
 
 // uncomment one combo 9-DoF!
-//#include "LSM6DS_LIS3MDL.h"  // can adjust to LSM6DS33, LSM6DS3U, LSM6DSOX...
-#include "LSM9DS.h"           // LSM9DS1 or LSM9DS0
+#include "LSM6DS_LIS3MDL.h"  // can adjust to LSM6DS33, LSM6DS3U, LSM6DSOX...
+//#include "LSM9DS.h"           // LSM9DS1 or LSM9DS0
 //#include "NXP_FXOS_FXAS.h"  // NXP 9-DoF breakout
 
 Adafruit_Sensor_Calibration cal;
