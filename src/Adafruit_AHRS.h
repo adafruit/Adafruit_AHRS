a6eba1cb7b4529e1679d8569498061155263caa7
ToMe25 <38815969+ToMe25@users.noreply.github.com>
2021-02-09 17:41:23 +0100

    Add some doxygen documentation (#19)
    * Add some initial NXP docu
    
    gitignore the html folder
    I feel like its better to commit the Doxyfile, as i had to add the src
    folder as an input for it to generate anything.
    
    * Add Simple AHRS documentation
    
    Slightly improve existing NXP Fusion docu
    
    * Formatting
    
    * Minor Fixes
    
     * Fix mainpage
     * Document some classes correctly
    
    * Delete Doxyfile

--- src/Adafruit_AHRS.h
+++ src/Adafruit_AHRS.h
@@ -1,3 +1,21 @@
+/*!
+ * @file Adafruit_AHRS
+ *
+ * @mainpage Adafruit AHRS
+ *
+ * @section intro_sec Introduction
+ *
+ * This library lets you take an accelerometer, gyroscope and magnetometer
+ * and combine the data to create orientation data.
+ *
+ * Options are Mahony (lowest memory/computation),
+ * Madgwick (fair memory/computation), and NXP fusion/Kalman (highest).
+ *
+ * While in theory these can run on an Arduino UNO/Atmega328P we really
+ * recommend a SAMD21 or better. Having single-instruction floating point
+ * multiply and plenty of RAM will help a lot!
+ */
+
 #ifndef __ADAFRUIT_AHRS_H_
 #define __ADAFRUIT_AHRS_H_
 
