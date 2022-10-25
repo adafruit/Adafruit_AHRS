c839abe971c832900c715e3bd49ad883bd6bbec5
id-run <116562015+id-run@users.noreply.github.com>
2022-10-25 16:44:51 +0200

    Make the library compatible with non-Arduino projects
    Add imports and remove hard Arduino.h dependency

--- src/Adafruit_Sensor_Set.h
+++ src/Adafruit_Sensor_Set.h
@@ -2,6 +2,7 @@
 #define __ADAFRUIT_SENSOR_SET_H__
 
 #include <Adafruit_Sensor.h>
+#include <stddef.h>
 
 // Interface for a device that has multiple sensors that can be queried by type.
 class Adafruit_Sensor_Set {
